9568d7e9f98132aeee56e84cba066fd9e31b2714
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-20 22:06:21 +0800

    task: Create scaffolding task: Create appbar task: add asset image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+/*
+    John Olan S. Gomez
+    Software Engineer Intern
+    I Am Poor App Challenge
+    January 20, 2024
+ */
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        title: const Text('I Am Poor', style: TextStyle(color: Colors.white)),
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/sad-ant.gif'),
+        ),
+      ),
+    ),
+  ));
 }
